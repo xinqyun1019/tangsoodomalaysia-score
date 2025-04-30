0e3d3a4c941a85028e59e930f5247136d4dd2a27
xinqyun1019 <ngxingyun85@gmail.com>
2025-04-30 09:42:23 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "offline-cache-v2"; // 3.2.214
+const CACHE_NAME = "offline-cache-v2"; // 3.2.217
 const urlsToCache = [
     "/tangsoodomalaysia-score",
     "/tangsoodomalaysia-score/index.html",
